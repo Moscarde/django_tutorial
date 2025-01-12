aa9a5e732cb5f3ea41c802a3ecf16cce7b628708
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-12 10:45:46 -0300

    feat: Implement index and detail views with templates for polls app

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,33 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+
 from .models import Question
+
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+
+    context = {"latest_question_list": latest_question_list}
+
+    # # Utilizando HttpResponse
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
+
+    # Utilizando render
+    return render(request, "polls/index.html", context)
+
 
 def detail(request, question_id):
-    return HttpResponse("Você está olhando para a questão %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
 
 def results(request, question_id):
     response = "Você está olhando para os resultados das questões %s"
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("Você está votando na questão %s" % question_id)
