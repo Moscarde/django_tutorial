8372638e4a7b1acafa9cd1d487b8d47cb567d6b4
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-12 15:26:50 -0300

    feat: Refactor views to use class-based views for index, detail, and results in polls app

--- polls/views.py
+++ polls/views.py
@@ -3,31 +3,52 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
+# # Verbose views
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
 
-    context = {"latest_question_list": latest_question_list}
+#     # # Utilizando HttpResponse
+#     # template = loader.get_template("polls/index.html")
+#     # return HttpResponse(template.render(context, request))
 
-    # # Utilizando HttpResponse
-    # template = loader.get_template("polls/index.html")
-    # return HttpResponse(template.render(context, request))
-
-    # Utilizando render
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+#     # Utilizando render
+#     return render(request, "polls/index.html", context)
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,"polls/results.html", {"question": question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,"polls/results.html", {"question": question})
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list" 
+    # força a utilizar uma variável de contexto customizada
+    # padrão é {model.name}_list
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
